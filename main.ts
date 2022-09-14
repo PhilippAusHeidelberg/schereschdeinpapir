06bfa4faa1332e94c33099a5e1d9519cf94fa2ab
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-09-14 19:10:54 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,31 @@
+let philipp = 0
+input.onButtonPressed(Button.A, function () {
+    philipp = randint(1, 3)
+    if (philipp == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (philipp == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    if (philipp == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
